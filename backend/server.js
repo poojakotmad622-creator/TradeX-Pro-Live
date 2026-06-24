41a4f4a35d59f2daff3cab67d84b76f8125f1f40
geetakotmad <kotmadgeeta@gmail.com>
2026-06-24 16:59:56 +0530

    Fix Railway port

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
