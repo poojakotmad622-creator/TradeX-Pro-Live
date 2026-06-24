058055a54c942eea7f8010954ee25f32933d6bc1
geetakotmad <kotmadgeeta@gmail.com>
2026-06-24 18:55:59 +0530

    Debug MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI);
+    console.log("Connecting to MongoDB...");
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
   } catch (error) {
-  console.error("❌ Full MongoDB Error:");
-  console.error(error);
-}
+    console.error("❌ MongoDB Error:");
+    console.error(error);
+    process.exit(1);
   }
+};
 
 module.exports = connectDB;
