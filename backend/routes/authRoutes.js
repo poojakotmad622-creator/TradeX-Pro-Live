1ccc3613d6e8968aa3ac804a7d88c2c21bcdf3bb
geetakotmad <kotmadgeeta@gmail.com>
2026-06-26 14:37:34 +0530

    remove temporary make admin route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/User");
-
 const authMiddleware = require("../middleware/authMiddleware");
 
 const {
@@ -19,36 +17,5 @@ router.post("/login", loginUser);
 
 // Profile
 router.get("/profile", authMiddleware, getProfile);
-
-// Temporary Make Admin
-router.get("/make-admin", async (req, res) => {
-  try {
-
-    const user = await User.findOneAndUpdate(
-      { email: "pooja@test.com" },
-      {
-        role: "admin",
-        createdBy: null
-      },
-      { new: true }
-    );
-
-    if (!user) {
-      return res.status(404).json({
-        message: "User not found"
-      });
-    }
-
-    res.status(200).json({
-      message: "Admin created successfully",
-      user
-    });
-
-  } catch (error) {
-    res.status(500).json({
-      message: error.message
-    });
-  }
-});
 
 module.exports = router;
